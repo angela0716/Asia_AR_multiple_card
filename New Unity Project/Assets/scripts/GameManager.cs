5da445e40652dcfc2a9f1b5f267c7cc0664aa3e1
angela0716 <72055214+angela0716@users.noreply.github.com>
2020-11-03 12:04:53 +0800

    旋轉縮放控制器

--- New Unity Project/Assets/scripts/GameManager.cs
+++ New Unity Project/Assets/scripts/GameManager.cs
@@ -2,6 +2,26 @@
 
 public class GameManager : MonoBehaviour
 {
+    [Header ("獅子")]
     public Transform lion;
+    [Header ("企鵝")]
     public Transform penguin;
+    [Header("joystick")]
+    public FixedJoystick joystick;
+    [Header("旋轉速度"),Range(0.1f,20f)]
+    public float turn = 1.5f;
+    [Header("縮放"), Range(0f, 5f)]
+    public float size = 0.3f;
+    private void Start()
+    {
+        print("開始事件");   
+    }
+    private void Update()
+    {
+        print("更新事件");
+        print(joystick.Vertical);
+        lion.Rotate(0, joystick.Horizontal*turn , 0);
+        penguin.Rotate(0, joystick.Horizontal * turn, 0);
+        lion.localScale += new Vector3(1, 1, 1) * joystick.Vertical*size;
+    }
 }
